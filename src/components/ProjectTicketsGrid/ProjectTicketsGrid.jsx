bdc4f6f12ef0c3f0c006f9c87fb6bba2a672ceef
andranikdodoyan <dodoyan2014@gmail.com>
2024-05-21 16:18:10 +0400

    assignedUser

--- src/components/ProjectTicketsGrid/ProjectTicketsGrid.jsx
+++ src/components/ProjectTicketsGrid/ProjectTicketsGrid.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './ProjectTicketsGrid.css';
+import axios from "axios";
+
 
 
 const ProjectTicketsGrid = ({ tickets }) => {
+    const [userMap, setUserMap] = useState({});
+
+    useEffect(() => {
+      const fetchUserInfo = async (assigneeId) => {
+        try {
+          const response = await axios.get(`http://localhost:8080/user/${assigneeId}`);
+          return response.data;
+        } catch (error) {
+          console.error(`Error fetching user info for ID ${assigneeId}:`, error);
+          return null;
+        }
+      };
+  
+      const fetchAllUserInfos = async () => {
+        const userInfoPromises = tickets.map(ticket => fetchUserInfo(ticket.assigneeId));
+        const userInfos = await Promise.all(userInfoPromises);
+  
+        const newUserMap = tickets.reduce((acc, ticket, index) => {
+          acc[ticket.assigneeId] = userInfos[index];
+          return acc;
+        }, {});
+  
+        setUserMap(newUserMap);
+      };
+  
+      fetchAllUserInfos();
+    }, [tickets]);
 
 
-    const handleSaveClick = (ticketId) => {
+    const handleEditClick = (ticketId) => {
         // Handle save click for the ticket with the given ID
-        console.log(`Save clicked for ticket ${ticketId}`);
+        console.log(`Edit clicked for ticket ${ticketId}`);
       };
       return (
       <div className="grid-container">
@@ -16,9 +45,11 @@ const ProjectTicketsGrid = ({ tickets }) => {
             {tickets.map((ticket, index) => (
             <div key={index} className="ticket-card">
                 <h3>{ticket.headline}</h3>
-                <p><strong>Assignee:</strong> {ticket.assignee}</p>
+                <p><strong>Assignee:</strong> {userMap[ticket.assigneeId]?.firstName} {userMap[ticket.assigneeId]?.lastName}</p>
                 <p><strong>Status:</strong> {ticket.status}</p>
-                <button onClick={() => handleSaveClick(ticket.id)}>Save</button>
+                <p><strong>Body:</strong> {ticket.body}</p>
+
+                <button onClick={() => handleEditClick(ticket.id)}>Edit</button>
                 </div>
             ))}
             </div>
