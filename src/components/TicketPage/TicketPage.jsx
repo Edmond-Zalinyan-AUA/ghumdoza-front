7edac99e0c839eccdb5f62aad2f4895c63e77e71
Edmond <edmond.zalinyan@wmg.com>
2024-05-23 12:12:08 +0400

    Ticket Page design

--- src/components/TicketPage/TicketPage.jsx
+++ src/components/TicketPage/TicketPage.jsx
@@ -5,9 +5,18 @@ import axios from 'axios';
 const TicketPage = ({ userId, ticket, setTicket, userAlltasks, setTicketsInMenu }) => {
 
     const [ticketAssignee, setTicketAssignee] = useState({
+        id: '',
         firstName: '',
         lastName: ''
     });
+    const [editableTicketData, setEditableTicketData] = useState({
+        headline: '',
+        assigneeId: '',
+        body: '',
+        status: ''
+    });
+    const [participants, setParticipants] = useState([]);
+    const [editMode, setEditMode] = useState(false);
 
     useEffect(() => {
         axios.get(`http://localhost:8080/user/${ticket.assigneeId}`)
@@ -17,25 +26,113 @@ const TicketPage = ({ userId, ticket, setTicket, userAlltasks, setTicketsInMenu 
             .catch(error => { console.error('Error fetching ticket assignee:', error); });
     }, [ticket]);
 
+    useEffect(() => {
+        axios.get('http://localhost:8080/project/participants/' + ticket.projectId)
+            .then(response => {
+                setParticipants(response.data);
+            })
+            .catch(error => console.error('Error fetching participants:', error));
+    }, [ticket]);
+
+    const handleEditClick = () => {
+        setEditMode(true);
+        setEditableTicketData({
+            ticketId: ticket.ticketId,
+            status: ticket.status,
+            body: ticket.body,
+            assigneeId: ticket.assigneeId
+        });
+    };
+
+    const handleSaveClick = () => {
+        axios.post('http://localhost:8080/ticket/update', editableTicketData)
+            .then(response => {
+                if (response.status === 200) {
+                    setTicket(response.data);
+                    if (editableTicketData.assigneeId !== userId) {
+                        setTicketsInMenu([...userAlltasks].filter(t => t.ticketId !== response.data.ticketId));
+                    }
+                }
+            })
+            .catch(error => console.error('Error editing ticket:', error));
+
+        // Reset the editable state
+        setEditMode(false);
+        setEditableTicketData({});
+    };
+
+    const handleTicketEditInputChange = (field, value) => {
+        setEditableTicketData(prevState => ({
+            ...prevState,
+            [field]: value
+        }));
+    };
+
+
     return (
         <div>
             <div className="ticket-headline">
                 <h1>{ticket.headline}</h1>
                 <br />
-                <div className="participant-grid">
-                </div>
             </div>
             <div className="ticket-assignee">
-                <h1>Assignee: {ticketAssignee.firstName + ' ' + ticketAssignee.lastName}</h1>
                 <br />
-                <div className="participant-grid">
-                </div>
+                {editMode ? (
+                    <>
+                        <h1><strong>Assignee:  </strong>
+                            <select
+                                className='select-assignee'
+                                name="assignee"
+                                value={editableTicketData.assigneeId}
+                                onChange={(e) => handleTicketEditInputChange('assigneeId', e.target.value,)}
+                                required
+                            >
+                                <option value="" disabled>Select assignee</option>
+                                {participants.map(participant => (
+                                    <option value={participant.user.id}>
+                                        {participant.user.firstName} {participant.user.lastName}
+                                    </option>
+                                ))}
+                            </select>
+                        </h1>
+                        <br />
+                        <h1><strong>Status:  </strong>
+                            <select
+                                className='select-status'
+                                value={editableTicketData.status}
+                                onChange={(e) => handleTicketEditInputChange('status', e.target.value)}
+                            >
+                                <option value="TO_DO">TO DO</option>
+                                <option value="IN_PROGRESS">IN PROGRESS</option>
+                                <option value="DONE">DONE</option>
+                            </select>
+                        </h1>
+                    </>
+                ) : (
+                    <>
+                        <h1><strong>Assignee:</strong> {ticketAssignee.firstName + ' ' + ticketAssignee.lastName}</h1>
+                        <br />
+                        <h1><strong>Status:</strong> {ticket.status}</h1>
+                    </>
+                )}
             </div>
             <div className="ticket-body">
-                <h1>{ticket.body}</h1>
+                {editMode ? (
+                    <>
+                        <textarea className="ticket-body-text"
+                            value={editableTicketData.body}
+                            onChange={(e) => handleTicketEditInputChange('body', e.target.value)}
+                        />
+                    </>
+                ) : (
+                    <>
+                        <p>{ticket.body}</p>
+                    </>
+                )}
                 <br />
-                <div className="participant-grid">
-                </div>
+                <button className="edit-button" onClick={() => editMode ? handleSaveClick() : handleEditClick()}>
+                    {editMode ? 'Save' : 'Edit'}
+                </button>
             </div>
         </div>
 
